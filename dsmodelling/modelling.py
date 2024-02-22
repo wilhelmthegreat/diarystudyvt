914704b1c8ab82a10c782045744f549ceeac4464
Phillip <phillip.a.jordan@gmail.com>
2024-02-21 19:31:08 -0500

    minor change to modelling script

--- dsmodelling/modelling.py
+++ dsmodelling/modelling.py
@@ -1,6 +1,9 @@
 import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 def wordcloud(text):
-    words = nltk.tokenize.word_tokenize(text, num)
+    words = nltk.tokenize.word_tokenize(text)
     stopwords = nltk.corpus.stopwords.words('english')
     wordDist = nltk.FreqDist(w.lower() for w in words if w not in stopwords)
     return(wordDist.most_common(num))
