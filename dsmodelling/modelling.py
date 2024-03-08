e2f0dc002f6bf68358874764e0de92daa2346757
Phillip <phillip.a.jordan@gmail.com>
2024-03-07 20:49:24 -0500

    Updates to word cloud functionality
    Turned modeler into class, added function to add stopwords, and removed punctuation from tokenized list.

--- dsmodelling/modelling.py
+++ dsmodelling/modelling.py
@@ -2,8 +2,14 @@ import nltk
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
-def wordcloud(text):
-    words = nltk.tokenize.word_tokenize(text)
-    stopwords = nltk.corpus.stopwords.words('english')
-    wordDist = nltk.FreqDist(w.lower() for w in words if w not in stopwords)
-    return(wordDist.most_common(num))
+nltk.download('stopwords')
+tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
+class processor:
+    stpw = list()
+    def word_cloud(self, text, num):
+        words = tokenizer.tokenize(text)
+        stopwords = nltk.corpus.stopwords.words('english') + self.stpw
+        wordDist = nltk.FreqDist(w for w in words if w.lower() not in stopwords)
+        return(wordDist.most_common(num))
+    def add_stop_words(self, word):
+        self.stpw.append(word)
