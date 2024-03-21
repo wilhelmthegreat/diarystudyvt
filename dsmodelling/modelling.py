cffcb9fe679f207b58d8e7d636e96eeca5b3b7e1
Phillip <phillip.a.jordan@gmail.com>
2024-03-21 01:10:40 -0400

    nltk updates

--- dsmodelling/modelling.py
+++ dsmodelling/modelling.py
@@ -9,7 +9,9 @@ class processor:
     def word_cloud(self, text, num):
         words = tokenizer.tokenize(text)
         stopwords = nltk.corpus.stopwords.words('english') + self.stpw
-        wordDist = nltk.FreqDist(w for w in words if w.lower() not in stopwords)
+        words = [w for w in words if w.lower() not in stopwords]
+        pos_tags = nltk.pos_tag(words)
+        wordDist = nltk.FreqDist(pos_tags)
         return(wordDist.most_common(num))
     def add_stop_words(self, word):
         self.stpw.append(word)
