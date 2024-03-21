ba159478ccfcfef736e45a9af558713cc7e44c84
raditya2023F <adityarana2003.ar@gmail.com>
2024-03-21 12:15:47 -0400

    Update bertpc.py to add stop_words
    Can't really see much of the output on my end. Maybe we need to print it out to a .txt file to have a proper look at what the model gives us as output

--- dsmodelling/bertpc.py
+++ dsmodelling/bertpc.py
@@ -1,11 +1,25 @@
 from bertopic import BERTopic
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
 import json
 
-f = open('sample.json', encoding='utf8')
-sample= json.load(f)
+f = open("sample.json", encoding="utf8")
+sample = json.load(f)
 f.close()
 
-topic_model = BERTopic()
+# Adding stopwords from json file
+with open("smart-stop-words.json", "r") as f:
+    stopwords_data = json.load(f)
+
+# adding stopwords to vectorizer model
+vectorizer_model = CountVectorizer(stop_words=stopwords_data)
+
+topic_model = BERTopic(
+    vectorizer_model=vectorizer_model,  # added it to the model
+    nr_topics=5,
+    calculate_probabilities=True,
+    verbose=True,
+)
 topics, probs = topic_model.fit_transform(sample)
 
 print(topic_model.get_topic_info())
