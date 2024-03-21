143e985648004cd4361741fa6fe8d36bdc3c9b79
Techzjc <techzjc@vt.edu>
2024-03-21 11:44:26 -0400

    refactor: 💡 Make jwt_cert_creation as function
    Make scripts/jwt_cert_creation.py as a function to make it easier to
    call by other Python scripts

--- scripts/jwt_cert_creation.py
+++ scripts/jwt_cert_creation.py
@@ -11,48 +11,51 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
 import os
 
-private_key_file_name = "jwt_private.pem"
-public_key_file_name = "jwt_public.pem"
+def jwt_create(
+        export_path=os.path.join(os.path.dirname(__file__), "..", "config", "secret"),
+        private_key_file_name="jwt_private.pem", 
+        public_key_file_name="jwt_public.pem"
+    ):
+    # Check if the directory exists
+    if not os.path.exists(export_path):
+        print("Please make sure the directory exists.")
+        exit(1)
 
-# Set the export path
-export_path = os.path.join(os.path.dirname(__file__), "..", "config", "secret")
+    # Check if the public and private key files exist
+    if os.path.exists(os.path.join(export_path, public_key_file_name)) or os.path.exists(os.path.join(export_path, private_key_file_name)):
+        overwrite = input("The public and private key files already exist. Do you want to overwrite them? (y/n)")
+        if overwrite.lower() != "y" and overwrite.lower() != "yes":
+            print("Exiting...")
+            exit(0)
 
-# Check if the directory exists
-if not os.path.exists(export_path):
-    print("Please make sure the `config/secret` directory exists.")
-    exit(1)
+    key = rsa.generate_private_key(
+        backend=crypto_default_backend(),
+        public_exponent=65537,
+        key_size=2048
+    )
 
-# Check if the public and private key files exist
-if os.path.exists(os.path.join(export_path, public_key_file_name)) or os.path.exists(os.path.join(export_path, private_key_file_name)):
-    overwrite = input("The public and private key files already exist. Do you want to overwrite them? (y/n)")
-    if overwrite.lower() != "y" and overwrite.lower() != "yes":
-        print("Exiting...")
-        exit(0)
+    private_key = key.private_bytes(
+        crypto_serialization.Encoding.PEM,
+        crypto_serialization.PrivateFormat.PKCS8,
+        crypto_serialization.NoEncryption()
+    )
 
-key = rsa.generate_private_key(
-    backend=crypto_default_backend(),
-    public_exponent=65537,
-    key_size=2048
-)
+    public_key = key.public_key().public_bytes(
+        crypto_serialization.Encoding.PEM,
+        crypto_serialization.PublicFormat.SubjectPublicKeyInfo
+    )
 
-private_key = key.private_bytes(
-    crypto_serialization.Encoding.PEM,
-    crypto_serialization.PrivateFormat.PKCS8,
-    crypto_serialization.NoEncryption()
-)
+    # Write the keys to the files and set the permissions
+    with open(os.path.join(export_path, private_key_file_name), "wb") as file:
+        file.write(private_key)
+        os.chmod(os.path.join(export_path, private_key_file_name), 0o600)
 
-public_key = key.public_key().public_bytes(
-    crypto_serialization.Encoding.PEM,
-    crypto_serialization.PublicFormat.SubjectPublicKeyInfo
-)
-
-# Write the keys to the files and set the permissions
-with open(os.path.join(export_path, private_key_file_name), "wb") as file:
-    file.write(private_key)
-    os.chmod(os.path.join(export_path, private_key_file_name), 0o600)
-
-with open(os.path.join(export_path, public_key_file_name), "wb") as file:
-    file.write(public_key)
-    os.chmod(os.path.join(export_path, public_key_file_name), 0o644)
+    with open(os.path.join(export_path, public_key_file_name), "wb") as file:
+        file.write(public_key)
+        os.chmod(os.path.join(export_path, public_key_file_name), 0o644)
+    
+    print("The JWT private and public keys have been successfully created.")
 
 
+if __name__ == "__main__":
+    jwt_create()
