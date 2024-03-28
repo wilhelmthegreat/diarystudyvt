7cc00d48fee6c1cc915b17bd2e3307e07b4bc458
Phillip <phillip.a.jordan@gmail.com>
2024-03-28 02:40:59 -0400

    Updated requirements and started using pyplot

--- dsmodelling/sentiment.py
+++ dsmodelling/sentiment.py
@@ -14,5 +14,6 @@ for entry in dat:
         outputs = model(**tkns)
         logits = outputs.logits
         probabilities = torch.softmax(logits, dim=1)
-    sntmts.append(round(probabilities[0].tolist()[1]*100)/100)
+    base_values = probabilities[0].tolist()
+    sntmts.append(round((base_values[1]-base_values[0])*100)/100)
 print(sntmts)
