ce4b7fcc5e22f8fe6fbd43bb70a2719f2b1ceaf5
Phillip <phillip.a.jordan@gmail.com>
2024-03-28 11:37:42 -0400

    pushing new changes from home computer

---
+++ dsmodelling/LDA2.py
@@ -0,0 +1,31 @@
+def format_topics_sentences(ldamodel=None, corpus=corpus, texts=data):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row_list in enumerate(ldamodel[corpus]):
+        row = row_list[0] if ldamodel.per_word_topics else row_list            
+        # print(row)
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return(sent_topics_df)
+
+
+df_topic_sents_keywords = format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=data_ready)
+
+# Format
+df_dominant_topic = df_topic_sents_keywords.reset_index()
+df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+df_dominant_topic.head(10)
--- dsmodelling/main.py
+++ dsmodelling/main.py
@@ -1,5 +1,38 @@
 import matplotlib.pyplot as plt
+import json
+import nltk
+from wordcloud import WordCloud, ImageColorGenerator
 
-plt.plot([1,2,3,4], [1,4,9,16])
-plt.axis([0,6,0,20])
-plt.show()
+jsn = json.load(open(input("file containing json data: "), encoding="utf8"))
+
+mode = input("which kind of visualization would you like?\n[1] Word Cloud\n[2] AI analysis plot\nSelection: ")
+
+def pos_color (word, font_size, position, orientation, random_state=None, **kwargs):
+    match nltk.pos_tag([word])[0][1]:
+        case "NN":
+            return "rgb(255,0,0)"
+        case _:
+            return "rgb(0,255,0)"
+
+match mode:
+    case "1":
+        #wordcloud code
+        nltk.download('punkt')
+        nltk.download('wordnet')
+        nltk.download('omw-1.4')
+        nltk.download('stopwords')
+        tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
+        txt = '\n'.join(jsn)
+        """stpw = list()
+        stopwords = nltk.corpus.stopwords.words('english') + stpw
+        words = [w for w in tokenizer.tokenize(txt) if w.lower() not in stopwords]
+        wordDist = nltk.FreqDist(words)"""
+        wc = WordCloud().generate_from_text(txt)
+        plt.imshow(wc.recolor(color_func=pos_color, random_state=3), interpolation="bilinear")
+        plt.axis('off')
+        plt.show()
+    case "2":
+        #AI analysis code
+        plt.plot([1,2,3,4], [1,4,9,16])
+        plt.axis([0,6,0,20])
+        plt.show()
