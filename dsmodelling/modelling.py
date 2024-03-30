8115240c7e61d656dba85d59457ceeb3fa1c3cd5
Phillip <phillip.a.jordan@gmail.com>
2024-03-30 19:17:28 -0400

    Added function to find associated words
    New function finds frequency distribution of words in close proximity to a word passed as an argument.

--- dsmodelling/modelling.py
+++ dsmodelling/modelling.py
@@ -1,17 +1,26 @@
 import nltk
+from nltk.text import Text
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 nltk.download('stopwords')
 tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
 class processor:
-    stpw = list()
+    stpw = nltk.corpus.stopwords.words('english')
     def word_cloud(self, text, num):
         words = tokenizer.tokenize(text)
-        stopwords = nltk.corpus.stopwords.words('english') + self.stpw
-        words = [w for w in words if w.lower() not in stopwords]
-        pos_tags = nltk.pos_tag(words)
-        wordDist = nltk.FreqDist(pos_tags)
+        words = [w for w in words if w.lower() not in self.stpw]
+        wordDist = nltk.FreqDist(words)
         return(wordDist.most_common(num))
     def add_stop_words(self, word):
         self.stpw.append(word)
+    def associated_word_cloud(self, text, word, num):
+        txt = Text(tokenizer.tokenize(text))
+        con_list = txt.concordance_list(word, width=40)
+        acc = list()
+        for line in con_list:
+            acc+=line.left
+            acc+=line.right
+        words = [w for w in acc if w.lower() not in (self.stpw+[word])]
+        wordDist = nltk.FreqDist(words)
+        return(wordDist.most_common(num))
