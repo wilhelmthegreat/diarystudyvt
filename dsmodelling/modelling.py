bd96dbc58bbe7b288946b52586c17dbfee137895
Phillip <phillip.a.jordan@gmail.com>
2024-03-31 03:10:26 -0400

    Characteristic sentence extraction
    Added feature to extract the most characteristic sentence from an entry

--- dsmodelling/modelling.py
+++ dsmodelling/modelling.py
@@ -1,5 +1,8 @@
 import nltk
 from nltk.text import Text
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import textrank_algorithm
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
@@ -7,11 +10,17 @@ nltk.download('stopwords')
 tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
 class processor:
     stpw = nltk.corpus.stopwords.words('english')
+    def _dist_to_dict(self, l):
+        acc = dict()
+        for s in l:
+            acc[s[0]] = s[1]
+        return(acc)
     def word_cloud(self, text, num):
         words = tokenizer.tokenize(text)
         words = [w for w in words if w.lower() not in self.stpw]
         wordDist = nltk.FreqDist(words)
-        return(wordDist.most_common(num))
+        distObj = wordDist.most_common(num)
+        return(self._dist_to_dict(distObj))
     def add_stop_words(self, word):
         self.stpw.append(word)
     def associated_word_cloud(self, text, word, num):
@@ -23,4 +32,18 @@ class processor:
             acc+=line.right
         words = [w for w in acc if w.lower() not in (self.stpw+[word])]
         wordDist = nltk.FreqDist(words)
-        return(wordDist.most_common(num))
+        distObj = wordDist.most_common(num)
+        return(self._dist_to_dict(distObj))
+    def get_sentences(self, lst):
+        result = [textrank_algorithm.get_best_sentence(e) for e in lst]
+        return(result)
+    def get_sentences(self, lst, wrd):
+            result = []
+            for e in lst:
+                if (wrd in e):
+                    conc = Text(tokenizer.tokenize(e)).concordance_list(wrd, width=40)
+                    rslt = (' '.join(conc[0].left))+' '+wrd+' '+(' '.join(conc[0].right))
+                    result.append(rslt)
+                else:
+                    result.append(textrank_algorithm.get_best_sentence(e))
+            return result
